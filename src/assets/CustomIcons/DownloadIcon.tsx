d0a90a2051d7ba3d671ecb023ff1e72e9f7a8439
Aashay-Thakur <thakur.aashay@gmail.com>
2024-08-03 08:17:12 +0530

    Corrected Icon Issue

--- src/assets/CustomIcons/DownloadIcon.tsx
+++ src/assets/CustomIcons/DownloadIcon.tsx
@@ -1,18 +1,14 @@
 import { easeOut, m } from 'framer-motion';
 
+import { SvgIcon } from '@mui/material';
+
 interface DownloadIconProps {
 	active: boolean;
 }
 
 const DownloadIcon = ({ active = false }: DownloadIconProps) => {
 	return (
-		<svg
-			className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root"
-			focusable="false"
-			aria-hidden="true"
-			viewBox="0 0 24 24"
-			data-testid="GetAppSharpIcon"
-			style={{ overflow: 'hidden' }}>
+		<SvgIcon focusable="false" aria-hidden="true" viewBox="0 0 24 24" style={{ overflow: 'hidden' }}>
 			<m.path
 				initial={{ y: 0 }}
 				animate={
@@ -28,7 +24,7 @@ const DownloadIcon = ({ active = false }: DownloadIconProps) => {
 				}
 				d="M 19 9 h -4 V 3 H 9 v 6 H 5 l 7 7 Z"></m.path>
 			<path d="M 5 22 v 2 h 14 v -2 Z"></path>
-		</svg>
+		</SvgIcon>
 	);
 };
 
--- src/assets/CustomIcons/ExternalLink.tsx
+++ src/assets/CustomIcons/ExternalLink.tsx
@@ -1,23 +1,20 @@
 import { m } from 'framer-motion';
 
+import { SvgIcon } from '@mui/material';
+
 interface ExternalLinkProps {
 	active: boolean;
 }
 
 const ExternalLink = ({ active }: ExternalLinkProps) => {
 	return (
-		<svg
-			className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root"
-			focusable="false"
-			aria-hidden="true"
-			viewBox="0 0 24 24"
-			data-testid="OpenInNewSharpIcon">
+		<SvgIcon focusable="false" aria-hidden="true" viewBox="0 0 24 24">
 			<path d="M 19 19 H 5 V 5 h 7 V 3 H 3 v 18 h 18 v -9 h -2 Z"></path>
 			<m.path
 				initial={false}
 				animate={active ? { x: 2, y: -2 } : { x: 0, y: 0 }}
 				d="M 14 3 v 2 h 3.59 l -9.83 9.83 l 1.41 1.41 L 19 6.41 V 10 h 2 V 3 Z"></m.path>
-		</svg>
+		</SvgIcon>
 	);
 };
 
